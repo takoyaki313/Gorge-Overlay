79c1eda218b916347990f3f63707355e0ae46d18
Takoyaki <40759792+takoyaki313@users.noreply.github.com>
2021-11-09 02:46:22 +0900

    Update color_setting_main.js
    Test用の値のままだったのを修正

--- js/color_setting_main.js
+++ js/color_setting_main.js
@@ -52,7 +52,7 @@ $(document).on("click","input[name='sample-data-type']",function(){
   let local_data = color_data_local_load(LOCAL_SAVE_NAME);
   dammy();
   if(select_type === 'RW'){
-    $("#source").load("https://takoyaki313.github.io/Gorge-Overlay/Test-module.html #gorge-source", function(gorge_source) {
+    $("#source").load("https://takoyaki313.github.io/Gorge-Overlay/Gorge-Overlay2.html #gorge-source", function(gorge_source) {
     if(gorge_source == null){
     $("#source").append("読込みに失敗しました");
     console.log('Failed...');
@@ -67,7 +67,7 @@ $(document).on("click","input[name='sample-data-type']",function(){
     });
   }
   else if (select_type === 'FL') {
-    $("#source").load("https://takoyaki313.github.io/Gorge-Overlay/Test-module.html #fl-source", function(fl_source) {
+    $("#source").load("https://takoyaki313.github.io/Gorge-Overlay/Gorge-Overlay2.html #fl-source", function(fl_source) {
     if(fl_source == null){
     $("#source").append("読込みに失敗しました");
     console.log('Failed...');
@@ -82,7 +82,7 @@ $(document).on("click","input[name='sample-data-type']",function(){
     });
   }
   else if (select_type === 'PvE') {
-    $("#source").load("https://takoyaki313.github.io/Gorge-Overlay/Test-module.html #pve-source", function(pve_source) {
+    $("#source").load("https://takoyaki313.github.io/Gorge-Overlay/Gorge-Overlay2.html #pve-source", function(pve_source) {
     if(pve_source == null){
     $("#source").append("読込みに失敗しました");
     console.log('Failed...');
@@ -111,7 +111,7 @@ $(function(){
   $('.fl').css('display','none');
   $('.pve').css('display','none');
   $('.rw').css('display','flex');
-$("#source").load("https://takoyaki313.github.io/Gorge-Overlay/Test-module.html #gorge-source", function(gorge_source) {
+$("#source").load("https://takoyaki313.github.io/Gorge-Overlay/Gorge-Overlay2.html #gorge-source", function(gorge_source) {
 if(gorge_source == null){
 $("#source").append("読込みに失敗しました");
 console.log('Failed...');
